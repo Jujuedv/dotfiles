e2ea8358656ab4c19173973128c23d8a26c02f9e
Jujuedv <jujuedv@gmail.com>
2017-02-15 05:39:52 +0100

    removed . from name, since these are the dotfiles anyway

--- jorpedo/symlinks.sh
+++ jorpedo/symlinks.sh
@@ -2,7 +2,7 @@
 
 if [ -z ${SYMLINKS_REL+x} ]; then echo "this should not be run on its own"; exit 1; fi
 
-SYMLINKS_REL+=(".i3\0.config/i3")
-SYMLINKS_REL+=(".powerline\0.config/powerline")
+SYMLINKS_REL+=("i3\0.config/i3")
+SYMLINKS_REL+=("powerline\0.config/powerline")
 SYMLINKS_REL+=("compton\0.config/compton")
-SYMLINKS_REL+=(".fehbg\0.fehbg")
+SYMLINKS_REL+=("fehbg\0.fehbg")
--- jpcarch/symlinks.sh
+++ jpcarch/symlinks.sh
@@ -2,5 +2,5 @@
 
 if [ -z ${SYMLINKS_REL+x} ]; then echo "this should not be run on its own"; exit 1; fi
 
-SYMLINKS_REL+=(".i3\0.config/i3")
-SYMLINKS_REL+=(".powerline\0.config/powerline")
+SYMLINKS_REL+=("i3\0.config/i3")
+SYMLINKS_REL+=("powerline\0.config/powerline")
